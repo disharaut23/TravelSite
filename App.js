a21589f9c5e546f579bac76e48145c2025f90ca0
disharaut23 <160864576+disharaut23@users.noreply.github.com>
2024-11-18 18:38:56 +0530

    Add files via upload
    TravelSite is a modern, responsive travel website developed using React, designed to provide users with a seamless experience in exploring and planning their trips. This project demonstrates a clean and user-friendly interface, dynamic components, and state management, making it an ideal starting point for travel-related web applications.

---
+++ App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Navigation  from './components/Navigation';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className='App'>
+     <Navigation/>
+      <Home/>
+      <About/>
+      <Services/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
